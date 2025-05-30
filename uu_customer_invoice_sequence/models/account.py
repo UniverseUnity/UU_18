2368e1a352dce29da5ca9bac35d1486ed43a2383
Vrushaparva <vrushparva27@gmail.com>
2025-05-30 10:06:08 +0530

    [FIX]Fixed the invoice error

--- uu_customer_invoice_sequence/models/account.py
+++ uu_customer_invoice_sequence/models/account.py
@@ -1,13 +1,12 @@
 from odoo import models, fields, api
 from datetime import datetime
 
-
 class AccountMove(models.Model):
     _inherit = "account.move"
 
     @api.model
     def create(self, vals):
-        if vals.get("move_type") == "out_invoice" and not vals.get("name") or vals["name"] == "/":
+        if vals.get("move_type") == "out_invoice" and (not vals.get("name") or vals.get("name") == "/"):
             sequence_id = self.env.ref("uu_customer_invoice_sequence.seq_invoice_custom")
             current_year = datetime.now().year
             sequence_id.prefix = f"UUS/{current_year}/"
